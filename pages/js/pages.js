968e77af70366ab23e22c2636e361ff722568a04
VMEniko <m0r0cz.3n1k0@gmail.com>
2022-06-03 02:09:03 +0200

    modified:   pages/js/pages.js

--- pages/js/pages.js
+++ pages/js/pages.js
@@ -2,11 +2,6 @@ function goBack() {
     window.history.back()
   };
 
-let agree = document.getElementById("agr")
-    agree.addEventListener("!IsChecked", function(){
-      alert("YOU MAY PAY YOUR SACRIFICES")
-    });
-
 function sacrifice(){
   
   let checkBox = document.getElementById("agr") 
